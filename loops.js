0b241699369d5e77665274d6b5e6acfcccdd44e2
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-30 01:04:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,12 @@ function whileLoop(num){
 }
 
 function doWhileLoop(int){
+  
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
   do{
     console.log('I run once regardless.')
   } while (incrementVariable() < int)
