e7d4f4d0fbcf8d11bcfb29b0f2f5c15340e73acb
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-30 00:30:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@ function forLoop(array){
   let output = [];
 
   for (let p = 0 ; i < 25 ; i++){
-    output.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+    output.push(`I am ${p} strange loop${p === 0 ? '' : 's'}.`)
   }
 
   return output;
